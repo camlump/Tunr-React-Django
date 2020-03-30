8cc9d97b64ef586431151303cb82ac2e482e47d1
CameronLumpkinATL <camlump@yahoo.com>
2020-03-30 14:29:23 -0400

    implement REST API on Django

--- tunr_app/urls.py
+++ tunr_app/urls.py
@@ -1,6 +1,13 @@
 # tunr_app/urls.py
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('artists', views.ArtistView)
+router.register('songs', views.SongView)
+
 urlpatterns = [
+    path('', include(router.urls))
+    
 ]
