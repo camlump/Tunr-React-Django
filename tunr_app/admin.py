6874dda3888da22750ad78af86d8d0ab231ee2ad
CameronLumpkinATL <camlump@yahoo.com>
2020-03-30 12:24:10 -0400

    added data throught admin

--- tunr_app/admin.py
+++ tunr_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artist, Song
+
+
+admin.site.register([Artist, Song])
 
 # Register your models here.
--- tunr_project/settings.py
+++ tunr_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_extensions',
     'tunr_app',
-    'tunr'
+    
 ]
 
 MIDDLEWARE = [
